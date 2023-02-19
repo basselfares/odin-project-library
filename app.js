039c0c500c0052d9e5870cb0a8c50630c8503009
Bassel Fares <basselmfares@gmail.com>
2023-02-19 02:16:30 -0500

    updated adding books and set up deleting books

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const title = document.getElementById('title')
 const author = document.getElementById('author')
 const pages = document.getElementById('pages')
 const read = document.getElementById('read')
+const container2 = document.querySelector('.container2')
+let button = document.querySelectorAll('button')
+
 
 const library = [];
 
@@ -16,20 +19,26 @@ function book(title, author, pages, read) {
 
 myForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log('Form has been submitted')
     library.push(new book(title.value, author.value, pages.value, read.value))
-    console.log(JSON.stringify(library))
     add()
+    button = document.querySelectorAll('button')
 })
 
 
 function add() {
     const div = document.createElement('div')
+    div.classList = `book${library.length}`
     div.innerHTML = `
     <div> Title: ${JSON.stringify(library[library.length-1].title).slice(1, -1)} </div>
     <div> Author: ${JSON.stringify(library[library.length-1].author).slice(1, -1)} </div>
     <div> Pages: ${JSON.stringify(library[library.length-1].pages).slice(1, -1)} </div>
     <div> Read: ${JSON.stringify(library[library.length-1].read).slice(1, -1)} </div>
+    <button class="book${library.length}" onClick="deleteButton('book${library.length}')">Delete</button>
     `
     document.querySelector('.container2').appendChild(div)
+}
+
+
+function deleteButton(className) {
+    document.getElementsByClassName(className)[0].remove()
 }
